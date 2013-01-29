3d596c056edf32fe781056c79c434a868f32903e
susan <susan@susan-T420s.(none)>
2013-01-28 20:50:53 -0800

    Still moving things over

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,25 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from blog.models import Entry
+from django.template import RequestContext
 
 def index(request):
     entry_info = Entry.objects.all()
-    return render_to_response('blog/preview.html',{'entries':entry_info})
+    return render_to_response('blog/preview.html',
+                                {'entries':entry_info},
+                                context_instance=RequestContext(request))
 
 def about(request):
-    return render_to_response('about.html')
+    return render_to_response('about.html',
+                                context_instance=RequestContext(request))
 
 def main(request):
-    return render_to_response('landing.html')
+    return render_to_response('landing.html',
+                                 context_instance=RequestContext(request))
 
 def entry(request, entry_id):
     entry_info = Entry.objects.get(id=entry_id)
-    return render_to_response('blog/entry.html',{'entry':entry_info})
+    return render_to_response('blog/entry.html',
+                                {'entry':entry_info},
+                                 context_instance=RequestContext(request))
 
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +13,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^/?$','pybyte.blog.views.main'),
     url(r'^about/$','pybyte.blog.views.about'),
