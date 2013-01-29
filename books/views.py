96112e2fc8b6b9f10daae0ae6d3d6a3ba9ba7265
susan <susan@susan-T420s.(none)>
2013-01-28 21:26:41 -0800

    using contextrequest to get correct static path.

--- books/views.py
+++ books/views.py
@@ -2,5 +2,6 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('books/index.html')
+    return render_to_response('books/index.html',
+                                context_instance=RequestContext(request))
 
--- feeds/views.py
+++ feeds/views.py
@@ -2,5 +2,6 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('feeds/index.html')
+    return render_to_response('feeds/index.html',
+                                 context_instance=RequestContext(request))
 
